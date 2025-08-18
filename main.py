14b3d6a4da9ae6d0fc42dcc6c99ad23408161569
AO3828 <ongktph@gmail.com>
2025-08-18 14:25:54 +0800

    Update main.py - 20th version

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import hmac
 import os
 import streamlit as st
 from streamlit_option_menu import option_menu
@@ -124,27 +123,6 @@ def show_onboarding_guidance(week, qa_chain=None):
 #                st.error("Invalid username or password.")
 
 
-def check_password():  
-    """Returns `True` if the user had the correct password."""  
-    def password_entered():  
-        """Checks whether a password entered by the user is correct."""  
-        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):  
-            st.session_state["password_correct"] = True  
-            del st.session_state["password"]  # Don't store the password.  
-        else:  
-            st.session_state["password_correct"] = False  
-    # Return True if the passward is validated.  
-    if st.session_state.get("password_correct", False):  
-        return True  
-    # Show input for password.  
-    st.text_input(  
-        "Password", type="password", on_change=password_entered, key="password"  
-    )  
-    if "password_correct" in st.session_state:  
-        st.error("😕 Password incorrect")  
-    return False
-
-
 # Simple login screen
 #def login():
 #    st.title("🔐 Login to AskITBuddy")
@@ -170,7 +148,7 @@ def main():
     #    login()
     #    return
 
-    if not check_password():  
+    if not utility.check_password():  
         st.stop()
 
     with st.sidebar:
@@ -259,6 +237,7 @@ def main():
 # Run it
 if __name__ == "__main__":
     main()
+
 
 
 
