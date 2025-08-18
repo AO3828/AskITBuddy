15eac4568b784027bffa8e12b97772d2ce8abc87
AO3828 <ongktph@gmail.com>
2025-08-18 14:04:53 +0800

    Update main.py - 16th version

--- main.py
+++ main.py
@@ -179,9 +179,10 @@ def main():
         for root, dirs, files in os.walk("data"):
             for file in files:
                 filename=os.path.join(root, file)
+                st.markdown(filename)
                 filelist.append(filename)
-
-        st.markdown(filelist)
+                
+        #st.markdown(filelist)
 
         st.write("")
         st.write("")
@@ -236,6 +237,7 @@ def main():
 # Run it
 if __name__ == "__main__":
     main()
+
 
 
 
